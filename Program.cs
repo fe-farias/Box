224c849fd296c052e51b859f90c45f3fc1c32bc3
Felipe Farias <fefarias97@gmail.com>
2021-08-22 20:19:20 -0300

    Resposta do exercício.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Felipe Francisco";
+            Console.WriteLine("+-------------------+");
+            Console.WriteLine("| "+nome+"  |");
+            Console.WriteLine("+-------------------+");
+        }
+    }
+}
